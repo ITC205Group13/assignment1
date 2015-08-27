6ba7eb5c468db8f2c127a5d5f77e3c32660a5950
Sjha02 <shae.jha@gmail.com>
2015-08-27 23:24:48 +1000

    added my code

--- ITC515_assignment2/src/datamanagement/UnitManager.java
+++ ITC515_assignment2/src/datamanagement/UnitManager.java
@@ -1,75 +1,87 @@
-package datamanagement;
+package  datamanagement;
 
-import java.util.List;
-import org.jdom.*;
+import  java.util.List;
+import  org.jdom.*;
 
-public class UnitManager {
+public  class  UnitManager {
 
-	private static UnitManager self = null;
+	private  static  UnitManager self  =  null;
 
-	private UnitMap UM;
+	private  UnitMap  unitMap;
 
-	public static UnitManager UM() {
-		if (self == null)
-			self = new UnitManager();
-		return self;
+	public  static  UnitManager  UM() {
+
+		if  (self  ==  null)
+
+			self  =  new  UnitManager();
+
+		return  self;
 	}
 
-	private UnitManager() {
-		UM = new UnitMap();
+	private  UnitManager()  {
+
+		unitMap  =  new  UnitMap();
 	}
 
-	public IUnit getUnit(String uc) {
-		IUnit iu = UM.get(uc);
-		return iu != null ? iu : createUnit(uc);
+	public  IUnit  getUnit(String  unitCode)  {
+
+		IUnit  unit  =  unitMap.get(unitCode);
+
+		return  unit  !=  null  ?  unit  :  createUnit(unitCode);
 
 	}
 
-	private IUnit createUnit(String unitCode) {
+	private  IUnit  createUnit(String unitCode) {
 
-		IUnit iu;
+		IUnit  unit;
 
-		for (Element el : (List<Element>) XMLManager.getXML().getDocument()
+		for  (Element  el  :  (List<Element>)  XMLManager.getXML().getDocument()
 				.getRootElement().getChild("unitTable").getChildren("unit"))
-			if (unitCode.equals(el.getAttributeValue("uid"))) {
-				StudentUnitRecordList slist;
 
-				slist = null;
-				iu = new Unit(el.getAttributeValue("uid"),
-						el.getAttributeValue("name"), Float.valueOf(
-								el.getAttributeValue("ps")).floatValue(), Float
-								.valueOf(el.getAttributeValue("cr"))
-								.floatValue(), Float.valueOf(
+			if  (unitCode.equals(el.getAttributeValue("uid")))  {
+
+				StudentUnitRecordList studentlist;
+
+				studentlist = null;
+
+				unit  =  new  Unit(el.getAttributeValue("uid"),
+						el.getAttributeValue("name"),  Float.valueOf(
+								el.getAttributeValue("ps")).floatValue(),  Float
+						.valueOf(el.getAttributeValue("cr"))
+						.floatValue(),  Float.valueOf(
 								el.getAttributeValue("di")).floatValue(), Float
-								.valueOf(el.getAttributeValue("hd"))
-								.floatValue(), Float.valueOf(
-								el.getAttributeValue("ae")).floatValue(),
+						.valueOf(el.getAttributeValue("hd"))
+						.floatValue(),  Float.valueOf(
+								el.getAttributeValue("ae")). floatValue(),
 						Integer.valueOf(el.getAttributeValue("asg1wgt"))
-								.intValue(), Integer.valueOf(
+						.intValue(),  Integer.valueOf(
 								el.getAttributeValue("asg2wgt")).intValue(),
 						Integer.valueOf(el.getAttributeValue("examwgt"))
-								.intValue(), StudentUnitRecordManager
-								.instance().getRecordsByUnit(unitCode));
-				UM.put(iu.getUnitCode(), iu);
-				return iu;
+						.intValue(),  StudentUnitRecordManager
+						.instance().getRecordsByUnit(unitCode));
+				unitMap.put(unit.getUnitCode(),  unit);
+				return  unit;
 			}
 
-		throw new RuntimeException("DBMD: createUnit : unit not in file");
+		throw  new  RuntimeException("DBMD:  createUnit  :  unit  not  in  file");
 	}
 
-	public UnitMap getUnits() {
+	public  UnitMap  getUnits()  {
 
-		UnitMap uM;
-		IUnit iu;
+		UnitMap  unitMap;
+		IUnit  unit;
 
-		uM = new UnitMap();
-		for (Element el : (List<Element>) XMLManager.getXML().getDocument()
-				.getRootElement().getChild("unitTable").getChildren("unit")) {
-			iu = new UnitProxy(el.getAttributeValue("uid"),
+		unitMap  =  new UnitMap();
+		for  (Element  el  :  (List<Element>)  XMLManager.getXML().getDocument()
+				.getRootElement().getChild("unitTable").getChildren("unit"))  {
+
+			unit  =  new  UnitProxy(el.getAttributeValue("uid"),
 					el.getAttributeValue("name"));
-			uM.put(iu.getUnitCode(), iu);
+			unitMap.put(unit.getUnitCode(),  unit);
+
 		} // unit maps are filled with PROXY units
-		return uM;
+
+		return unitMap;
 	}
 
 }
